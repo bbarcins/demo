f89740bc39f11b8a17b9a88f26e03223df25dac6
Bartosz <b.barcinski@gmail.com>
2018-04-20 16:15:22 +0200

    initial commit

---
+++ src/git_demo.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : git_demo.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("This is Git Test");
+	return EXIT_SUCCESS;
+}
